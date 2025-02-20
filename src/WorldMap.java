735a3ab7d07745148f449f0f1d3799adacf4b21f
mrkos <mrkos@spsejecna.cz>
2025-02-20 11:05:40 +0100

    Pokus o vytvoreni metody na nacitani mist se Souboru

--- src/WorldMap.java
+++ src/WorldMap.java
@@ -1,2 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class WorldMap {
-}
+
+
+
+
+    public boolean loadMap(){
+        try (BufferedReader br = new BufferedReader(new FileReader("World.txt"))){
+                String line;
+                while ((line = br.readLine()) !=null){
+                    String[] lines = line.split("-");
+                }
+            } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+    }
+

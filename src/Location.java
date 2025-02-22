fadd241adfcd99ff154def0c49ad4b564ce290c9
MrkosOndra <mrkos@spsejecna.cz>
2025-02-22 22:27:24 +0100

    Vytvoreni  konstruktoru

--- src/Location.java
+++ src/Location.java
@@ -3,4 +3,9 @@ import java.util.HashSet;
 public class Location {
     private String name;
     private HashSet<Location> connections;
+
+    public Location(String name) {
+        this.name = name;
+        this.connections = new HashSet<>();
+    }
 }

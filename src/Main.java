c1fdeeedaf6a1ad1ccfa6709693ec3f8c58dbd9f
MrkosOndra <mrkos@spsejecna.cz>
2025-02-22 23:23:21 +0100

    metoda pro nacteni a vypsani mapy

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        WorldMap world= new WorldMap();
+        if(world.loadMap()){
+            world.printMap();
+        }
 
 
 

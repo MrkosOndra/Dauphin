5fa365ef2cde04c51b01be25e66cf616f1b7934d
MrkosOndra <mrkos@spsejecna.cz>
2025-02-22 22:33:49 +0100

    vytvoreni metody addConnections ktera prida pripopjeni mezi lokacemi a vygenerovani Getru a ToString metody

--- src/Location.java
+++ src/Location.java
@@ -8,4 +8,25 @@ public class Location {
         this.name = name;
         this.connections = new HashSet<>();
     }
+
+    public void addConnections(Location location){
+        connections.add(location);
+        location.connections.add(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HashSet<Location> getConnections() {
+        return connections;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "name='" + name + '\'' +
+                ", connections=" + connections +
+                '}';
+    }
 }

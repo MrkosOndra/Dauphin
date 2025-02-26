3f040798e33d64ed4307389654dba6eb0ebbafdf
MrkosOndra <mrkos@spsejecna.cz>
2025-02-26 11:36:24 +0100

    pridani metody exit

---
+++ src/Move.java
@@ -0,0 +1,29 @@
+public class Move extends Command{
+    public Move(WorldMap map) {
+        super(map);
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if(args.length>2) {
+            System.out.println("Použití: move <místo>");
+        }
+
+            String destination = args[1].toLowerCase();
+            Location nextLocation = currentLocation.getConnections().get(destination);
+
+            if (nextLocation != null) {
+                currentLocation = nextLocation;
+                System.out.println("Přesunul si se do: " + currentLocation.getName());
+            } else {
+                System.out.println("Tudy cesta nevede");
+            }
+
+
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
+    }
+}

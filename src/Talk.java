b52edf762f86b385a3acd92ff83de8003be3a412
mrkos <mrkos@spsejecna.cz>
2025-03-06 11:00:03 +0100

    dopsani execute metody pro tridu Talk.java tak aby hrac mohl mluvit s NPC v dane lokaci pokud tam takove je

--- src/Talk.java
+++ src/Talk.java
@@ -5,9 +5,18 @@ public class Talk extends Command{
 
     @Override
     public void execute(String[] args) {
-
+        if (args.length < 2){
+            System.out.println("pouziti: Talk <NPC_name>");
+            return;
+        }
+        String NpcName= args[1].toLowerCase();
+        NPC npc = map.getCurrentLocation().getNPC(NpcName);
+        if (npc!=null){
+            System.out.println(npc.getName() + ": " + npc.getDialogue());
+        }else{
+            System.out.println("Zde nikdo se jmenem"+ npc.getName()+ "neni!!");
+        }
     }
-
     @Override
     public boolean exit() {
         return false;

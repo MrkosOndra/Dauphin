c9cd6464ee3f727c48acbf2a670028cedf2745e0
mrkos <mrkos@spsejecna.cz>
2025-03-06 09:52:46 +0100

    pridani vypisu do metody execute ktere rikaji hraci ktere prikazy muze pouzit

--- src/Help.java
+++ src/Help.java
@@ -5,6 +5,13 @@ public class Help extends Command{
 
     @Override
     public void execute(String[] args) {
+        System.out.println("dostupne prikazy:" +
+                "move <misto> : pohnes se do mista kam chces (pokud tam vede cesta)");
+        System.out.println("Exit : Hra se ukonci");
+        System.out.println("Talk<NPCName>: zacnes mluvit s danou postavou");
+        System.out.println("Take: Seberes item po dokonceni ukolu");
+        System.out.println("Complete: dokoncis ukol");
+
 
     }
 

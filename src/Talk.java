2ff0871bf911252736d93b30cebee8fee7178231
MrkosOndra <mrkos@spsejecna.cz>
2025-03-10 21:19:11 +0100

    pouziti metod givetask a talk

--- src/Talk.java
+++ src/Talk.java
@@ -1,22 +1,28 @@
-public class Talk extends Command{
+public class Talk extends Command {
     public Talk(WorldMap map) {
         super(map);
     }
 
     @Override
     public void execute(String[] args) {
-        if (args.length < 2){
+        if (args.length < 2) {
             System.out.println("pouziti: Talk <NPC_name>");
             return;
         }
-        String NpcName= args[1].toLowerCase();
+        String NpcName = args[1].toLowerCase();
         NPC npc = map.getCurrentLocation().getNPC(NpcName);
-        if (npc!=null){
-            System.out.println(npc.getName() + ": " + npc.getDialogue());
-        }else{
-            System.out.println("Zde nikdo se jmenem"+ npc.getName()+ "neni!!");
+        if (npc != null) {
+            npc.talk();
+            Task task = npc.giveTask();
+            if (task != null) {
+                System.out.println("Úkol: " + task.getDescription());
+            } else {
+                System.out.println("Zde nikdo se jmenem" + npc.getName() + "neni!!");
+            }
         }
-    }
+
+        }
+
     @Override
     public boolean exit() {
         return false;

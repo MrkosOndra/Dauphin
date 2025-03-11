156bc84fbec4fd1d80ca682d6a87e9d63bf7c892
MrkosOndra <mrkos@spsejecna.cz>
2025-03-11 19:09:52 +0100

    vytvoreni inventare metodz pro pridani do inventare a take metodz ktera kontroluje zda hrac item vlastni a vypis inventare a pripraveniu ,metod pro vyhru

--- src/Player.java
+++ src/Player.java
@@ -1,23 +1,42 @@
 import java.util.List;
 
 public class Player {
-    private Location currentLocation;
     private List<Item> inventory;
-    private Task activeTask;
 
-    public Player(Location startLocation) {
-        this.currentLocation = startLocation;
+    public Player(List<Item> inventory, boolean hasWon) {
+        this.inventory = inventory;
+        this.HasWon = false;
     }
 
-    public void moveTo(Location location) {
+    private boolean HasWon;
 
-    }
 
     public void addItem(Item item) {
+        inventory.add(item);
+        System.out.println("sebral jsi: "+ item.getName());
 
     }
+    public boolean hasItem(String ItemName){
+        return inventory.stream().anyMatch(item-> item.getName().equalsIgnoreCase(ItemName));
+    }
 
-    public void completeTask(Task task) {
+    public void printInventory() {
+        if(inventory.isEmpty()){
+            System.out.println("Tvuj inventar je prazdny");
+        }else {
+            System.out.println("🎒 Tvůj inventář:");
+            for (Item item : inventory) {
+                System.out.println(" - " + item.getName());
+            }
+        }
 
+    }
+    public void winGame() {
+        this.HasWon = true;
+        System.out.println("🎉 Gratuluji! Dokončil jsi hru! 🎉");
+    }
+
+    public boolean hasWon() {
+        return HasWon;
     }
 }

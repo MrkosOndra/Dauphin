6a31a497c83dc323b345f73645f9dc981ca30608
MrkosOndra <mrkos@spsejecna.cz>
2025-03-11 19:31:27 +0100

    dopsani execute ted hrac muze po dokonceni ukolu sebrat item pokud bude v dane lokaci

--- src/Take.java
+++ src/Take.java
@@ -1,15 +1,30 @@
-public class Take extends Command{
+public class Take extends Command {
     public Take(WorldMap map) {
         super(map);
     }
 
     @Override
     public void execute(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Použití: take <předmět>");
+            return;
+        }
+        String itemName = args[1].toLowerCase();
+        Location currentLocation = map.getCurrentLocation();
+        Item item = currentLocation.getItem(itemName);
 
+        if(item!=null){
+            map.getPlayer().addItem(item);
+            currentLocation.removeItem(itemName);
+            System.out.println("sebral jsi:"+ item.getName());
+        }else{
+            System.out.println("tento item zde neni");
+        }
     }
 
-    @Override
-    public boolean exit() {
-        return false;
+        @Override
+        public boolean exit () {
+            return false;
+        }
     }
-}
+

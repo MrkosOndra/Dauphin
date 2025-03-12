a5031039580f677ac8a54facd6204b5ded9ff9af
MrkosOndra <mrkos@spsejecna.cz>
2025-03-12 12:41:21 +0100

    dopsani metody execute tak aby hrac mohl potom co bude mit potrebny item tak bude moct dokoncit task a ziskat dany pocet obyvatel

--- src/Complete.java
+++ src/Complete.java
@@ -5,7 +5,33 @@ public class Complete extends Command{
 
     @Override
     public void execute(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Použití: complete <jméno_npc>");
+            return;
+        }
 
+        String npcName = args[1];
+        NPC npc = map.getCurrentLocation().getNPC(npcName);
+
+        if (npc == null) {
+            System.out.println("Tento NPC zde není.");
+            return;
+        }
+
+        Task task = npc.giveTask();
+
+        if (task == null) {
+            System.out.println("Tento NPC nemá žádný úkol.");
+            return;
+        }
+
+        if (map.getPlayer().hasItem(task.getRequiredItem().getName())) {
+            map.getPlayer().addItem(task.getReward());
+            npc.getTasks().remove(task);
+            System.out.println("Úkol splněn! Dostal jsi: " + task.getReward().getName());
+        } else {
+            System.out.println("Nemáš potřebný předmět k dokončení úkolu.");
+        }
     }
 
     @Override

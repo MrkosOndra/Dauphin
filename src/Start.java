e171aeccbc3c821e48a63abcf04ae9d44df7514e
MrkosOndra <mrkos@spsejecna.cz>
2025-03-12 13:07:56 +0100

    pridani comandu do tridy Start.java

--- src/Start.java
+++ src/Start.java
@@ -1,37 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Start {
     private WorldMap world;
-    private Move move;
-    private Exit exit;
+    private Map<String, Command> commands;
     private Scanner scanner;
 
     public Start() {
         this.scanner = new Scanner(System.in);
         this.world = new WorldMap();
+        this.commands = new HashMap<>();
+
+        commands.put("move", new Move(world));
+        commands.put("exit", new Exit(world));
+        commands.put("talk", new Talk(world));
+        commands.put("take", new Take(world));
+        commands.put("complete", new Complete(world));
+        commands.put("help", new Help(world));
     }
 
     public void run() {
         if (world.loadMap()) {
             world.printMap();
             world.setStartLocation("Orleans");
-
-            this.move = new Move(world);
-            this.exit= new Exit(world);
-
-            System.out.println("Vítejte ve hře! Použijte příkaz 'move <místo>' pro pohyb nebo 'exit' pro ukončení.");
+            System.out.println("Vítejte ve hře!");
 
             while (true) {
                 System.out.print("\nZadejte příkaz: ");
                 String input = scanner.nextLine().trim().toLowerCase();
                 String[] commandArgs = input.split(" ");
 
-                if (commandArgs[0].equals("move")) {
-                    move.execute(commandArgs);
-                } else if (commandArgs[0].equals("exit")) {
-                   exit.execute(commandArgs);
+                if (commandArgs.length == 0) {
+                    System.out.println("Zadejte příkaz!");
+                    continue;
+                }
+
+                Command command = commands.get(commandArgs[0]);
+
+                if (command != null) {
+                    command.execute(commandArgs);
                 } else {
-                    System.out.println("Neznámý příkaz. Použijte 'move <místo>' nebo 'exit'.");
+                    System.out.println("Neznámý příkaz. Použijte 'help' pro seznam příkazů.");
                 }
             }
         }

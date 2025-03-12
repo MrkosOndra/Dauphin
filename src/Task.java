998332c7aa8057b7c2e32081d5974fdb3c4ada39
MrkosOndra <mrkos@spsejecna.cz>
2025-03-12 12:35:58 +0100

    dopsani tridy task a pridani vlastnosti kolik dostane obyvatel za splneni

--- src/Task.java
+++ src/Task.java
@@ -3,17 +3,35 @@ public class Task {
     private boolean complete;
     private Item requiredItem;
     private Item reward;
+    private int citizensReward;
 
-    public Task(String description, Item requiredItem, Item reward) {
+    public Task(String description, boolean complete, Item requiredItem, Item reward, int citizensReward) {
         this.description = description;
-        this.complete = false;
+        this.complete = complete;
         this.requiredItem = requiredItem;
         this.reward = reward;
+        this.citizensReward = citizensReward;
     }
 
     public boolean checkCompletion(Player player) {
+        if (player.hasItem(requiredItem.getName())) {
+            complete = true;
+            return true;
+        }
 
         return false;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public Item getRequiredItem() {
+        return requiredItem;
+    }
+
+    public int getCitizensReward() {
+        return citizensReward;
     }
 
     public Item getReward() {
